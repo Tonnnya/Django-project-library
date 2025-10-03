8f59c3bc297cfa9aaff3f335dd299466d68d80b5
Tonnnya <53794265+Tonnnya@users.noreply.github.com>
2025-10-03 15:55:27 +0300

    fix

--- library/order/consumers.py
+++ library/order/consumers.py
@@ -1,4 +1,7 @@
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Order
 
 class OrderNotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -6,4 +9,25 @@ class OrderNotificationConsumer(AsyncWebsocketConsumer):
 
         if self.user.is_authenticated:
             if self.user.role == 1:
-                self.group_name = 'librarian'
+                self.group_name = 'librarians'
+            else:
+                self.group_name = f'user_{self.user.id}'
+
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        if hasattr(self, 'group_name'):
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        pass
+
+    async def order_notification(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'order_notification',
+            'message': event['message'],
+            'order_id': event['order_id'],
+        }))
