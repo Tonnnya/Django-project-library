b632d40f39391cf2fae3749bec2851ccd7a6ade6
Tonnnya <53794265+Tonnnya@users.noreply.github.com>
2025-10-08 14:08:09 +0300

    fix

--- library/library/asgi.py
+++ library/library/asgi.py
@@ -10,14 +10,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from order import routing
 
